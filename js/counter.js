6784c56ac492f10e05d49949b839d4855a56ab5b
engineer-code2020 <64506248+engineer-code2020@users.noreply.github.com>
2020-05-30 04:35:14 +0200

    counf

--- js/counter.js
+++ js/counter.js
@@ -1,13 +1,13 @@
-var idy="top_english"
-var xhttp8 = new XMLHttpRequest();
-xhttp8.onreadystatechange = function() {
+var idCode="top_english"
+var xhttpCoun = new XMLHttpRequest();
+xhttpCoun.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
     
     }
 };
-xhttp8.open("POST", "https://breaktxt.000webhostapp.com/addView/", true);
-xhttp8.onload=function(){
+xhttpCoun.open("POST", "https://breaktxt.000webhostapp.com/addView/", true);
+xhttpCoun.onload=function(){
 
 }
-xhttp8.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-xhttp8.send("tableName="+idy);
+xhttpCoun.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhttpCoun.send("tableName="+idCode);
